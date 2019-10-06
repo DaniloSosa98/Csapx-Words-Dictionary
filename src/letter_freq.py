ad8dd4260e60c090febd14c010637bfda500a199
dgs5678 <dgs5678@g.rit.edu>
2019-10-05 23:07:58 -0400

    Letter Frequency is printed

--- src/letter_freq.py
+++ src/letter_freq.py
@@ -1,8 +1,11 @@
 import csv
 import argparse
 import sys
+from collections import Counter
+import string
 
 ERR = sys.stderr.write
+d = dict.fromkeys(string.ascii_lowercase, 0)
 
 def read_words(filename: str) -> dict:
     Wdict = {}
@@ -19,28 +22,55 @@ def read_words(filename: str) -> dict:
 
     return Wdict
 
-def Wcount(Wdict: dict, word: str) -> None:
+def total_count(Dtotal: dict) -> dict:
     count = 0
-    if word in Wdict:
-        for element in Wdict[word].get('occur'):
+    for word in Dtotal:
+        for element in Dtotal[word].get('occur'):
             count += int(element)
-        print('{} : {}'.format(word, count))
-    else:
-        ERR('Error: {} does not appear!\n\n'.format(word))
+        tempLet = Counter(word)
+        for letter in tempLet:
+            tempLet[letter] = tempLet.get(letter) * count
+
+        Dtotal[word] = {'total': count, 'letters': tempLet}
+
+
+    return Dtotal
+
+def letter_count(Dtotal: dict) -> None:
+    total = 0
+    for word in Dtotal:
+        for letter in Dtotal[word].get('letters'):
+            Ltotal =  Dtotal[word].get('letters').get(letter)
+
+            d[letter] = d.get(letter) + Ltotal
+            total += Ltotal
+
+    for letter in d:
+        d[letter] = d.get(letter)/total
 
 def main() -> None:
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("word", help="word to show its count")
-    parser.add_argument("filename", help="file to read from")
+    parser.add_argument("-o","--output", action="store_true", help="display letter frequencies to standard output")
+    parser.add_argument("-p","--plot", action="store_true", help="plot letter frequencies using matplotlib")
+    parser.add_argument("filename", help="a comma separated value unigram file")
     args = parser.parse_args()
-    word = args.word
     filename = ('../' + args.filename)
+
     if filename[-4:] != '.csv':
         ERR('Error: {} does not exist!\n\n'.format(args.filename))
     else:
         Wdict = read_words(filename)
-        Wcount(Wdict, word)
+        Wdict = total_count(Wdict)
+        letter_count(Wdict)
+
+        for key in d:
+            print(key, ':', d[key])
+
+    if args.output:
+        print('output')
+    if args.plot:
+        print('plot')
 
 if __name__ == '__main__':
     main()
